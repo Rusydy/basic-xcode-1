aa68413daee8533499e26f34e62c815399ce707c
Rusydy <rusydymuhiddin@gmail.com>
2023-02-23 15:05:07 +0700

    Initial Commit

---
+++ basic-xcode-1/basic_xcode_1App.swift
@@ -0,0 +1,17 @@
+//
+//  basic_xcode_1App.swift
+//  basic-xcode-1
+//
+//  Created by Rusydy Muhiddin on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct basic_xcode_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
